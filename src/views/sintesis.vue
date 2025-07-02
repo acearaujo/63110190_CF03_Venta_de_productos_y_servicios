af241674976764949ed51ad22fa244221e8806fd
acearaujo <hola@alvaroaraujo.com>
2025-07-02 15:37:42 -0500

    First commit

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
+    p El componente formativo Fidelización y postventa de productos y servicios se enfoca en fortalecer competencias clave para la atención y permanencia del cliente, mediante el uso de herramientas, estrategias y técnicas orientadas a la fidelización, evaluación de la satisfacción y gestión efectiva de quejas, reclamos y sugerencias, promoviendo así una mejora continua en la experiencia del cliente y relaciones comerciales duraderas desde un enfoque técnico y práctico. 
     .row.justify-content-center
       .col-lg-10.mb-5.bgs.p-4.brad
         figure
